94f0cd59ff94e4a3b7fe6778ae99d89eefd52101
Yaroslav_T <djemzee@inbox.ru>
2024-05-25 18:21:59 +0300

    ТЗ 5 Спринт, третья итерация. Обновил тесты, добавил несколько сущностей

--- src/tests/EpicTest.java
+++ src/tests/EpicTest.java
@@ -26,4 +26,27 @@ class EpicTest {
         epic.setStatus(Status.DONE);
         assertEquals(Status.DONE, epic.getStatus());
     }
+
+    @Test
+    void testUpdateStatus() {
+        Epic epic = new Epic("Эпик 1", "Описание эпика 1", 1, Status.NEW);
+        Subtask subtask1 = new Subtask("Подзадача 1", "Описание подзадачи 1", 2, Status.NEW, epic.getId());
+        Subtask subtask2 = new Subtask("Подзадача 2", "Описание подзадачи 2", 3, Status.NEW, epic.getId());
+
+        epic.addSubtask(subtask1);
+        epic.addSubtask(subtask2);
+
+        assertEquals(Status.NEW, epic.getStatus(), "Статус эпика должен быть NEW, если все подзадачи имеют статус NEW");
+
+        subtask1.setStatus(Status.DONE);
+        subtask2.setStatus(Status.DONE);
+        epic.setStatus(Status.DONE);
+
+        assertEquals(Status.DONE, epic.getStatus(), "Статус эпика должен быть DONE, если все подзадачи имеют статус DONE");
+
+        subtask2.setStatus(Status.IN_PROGRESS);
+        epic.setStatus(Status.IN_PROGRESS);
+
+        assertEquals(Status.IN_PROGRESS, epic.getStatus(), "Статус эпика должен быть IN_PROGRESS, если хотя бы одна подзадача не выполнена");
+    }
 }
--- src/tests/InMemoryTaskManagerTest.java
+++ src/tests/InMemoryTaskManagerTest.java
@@ -31,22 +31,53 @@ class InMemoryTaskManagerTest {
     }
 
     @Test
-    void testGetHistory() {
-        Task task1 = new Task("Задача 1", "Описание задачи 1", 1, Status.NEW);
-        Epic epic = new Epic("Эпик 1", "Описание эпика 1", 2, Status.NEW);
-        Subtask subtask = new Subtask("Подзадача 1", "Описание подзадачи 1", 3, Status.NEW, epic.getId());
-        taskManager.createTask(task1);
+    void testAddAndGetEpic() {
+        Epic epic = new Epic("Эпик 1", "Описание эпика 1", 1, Status.NEW);
         taskManager.createEpic(epic);
+        Epic retrievedEpic = taskManager.getEpic(epic.getId());
+        assertNotNull(retrievedEpic, "Эпик должен быть найден");
+        assertEquals(epic, retrievedEpic, "Эпики должны совпадать");
+    }
+
+    @Test
+    void testAddAndGetSubtask() {
+        Epic epic = new Epic("Эпик 1", "Описание эпика 1", 1, Status.NEW);
+        taskManager.createEpic(epic);
+
+        Subtask subtask = new Subtask("Подзадача 1", "Описание подзадачи 1", 2, Status.NEW, epic.getId());
         taskManager.createSubtask(subtask);
 
-        taskManager.getTask(task1.getId());
+        Subtask retrievedSubtask = taskManager.getSubtask(subtask.getId());
+        assertNotNull(retrievedSubtask, "Подзадача должна быть найдена");
+        assertEquals(subtask, retrievedSubtask, "Подзадачи должны совпадать");
+    }
+
+    @Test
+    void testGetHistory() {
+        Task task = new Task("Задача 1", "Описание задачи 1", 1, Status.NEW);
+        taskManager.createTask(task);
+        taskManager.getTask(task.getId());
+
+        Epic epic = new Epic("Эпик 1", "Описание эпика 1", 2, Status.NEW);
+        taskManager.createEpic(epic);
         taskManager.getEpic(epic.getId());
+
+        Subtask subtask = new Subtask("Подзадача 1", "Описание подзадачи 1", 3, Status.NEW, epic.getId());
+        taskManager.createSubtask(subtask);
         taskManager.getSubtask(subtask.getId());
 
         List<Task> history = taskManager.getHistory();
-        assertEquals(3, history.size());
-        assertEquals(task1, history.get(0));
-        assertEquals(epic, history.get(1));
-        assertEquals(subtask, history.get(2));
+        assertEquals(3, history.size(), "История должна содержать 3 задачи");
+        assertEquals(task, history.get(0), "Первой в истории должна быть 'Задача 1'");
+        assertEquals(epic, history.get(1), "Второй в истории должен быть 'Эпик 1'");
+        assertEquals(subtask, history.get(2), "Третьей в истории должна быть 'Подзадача 1'");
     }
+
+    @Test
+    void testTaskEqualityById() {
+        Task task1 = new Task("Задача 1", "Описание задачи 1", 1, Status.NEW);
+        Task task2 = new Task("Задача 1", "Описание задачи 1", 1, Status.NEW);
+        assertEquals(task1, task2, "Задачи с одинаковым id должны быть равны");
+    }
+
 }
